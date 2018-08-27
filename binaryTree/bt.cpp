193d2848a36cdc4e9fd6940cb7abd6b003c2da9a
Camila Herculano1 <cherculano1@Lenovo.com>
2018-08-27 19:43:38 -0300

    Initial commit

---
+++ binaryTree/bt.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+    void bfs(Node* current) {
+
+
+    }
+};
+
+int main() {
+  
+    Solution tree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = tree.insert(root, data);
+    }
+
+    return 0;
+}
